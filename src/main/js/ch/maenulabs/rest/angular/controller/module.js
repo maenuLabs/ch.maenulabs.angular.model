5d997dad5f58f5659f014865de107d4530ef5750
Manuel Leuenberger <maenulabs@gmx.ch>
2015-12-16 19:06:24 +0100

    add requires to modules

--- src/main/js/ch/maenulabs/rest/angular/controller/module.js
+++ src/main/js/ch/maenulabs/rest/angular/controller/module.js
@@ -3,6 +3,7 @@
  * The controller.
  *
  * @module ch.maenulabs.rest.angular.controller
+ * @requires ch.maenulabs.rest.angular.service
  */
 angular.module('ch.maenulabs.rest.angular.controller', [
 	'ch.maenulabs.rest.angular.service'
--- src/main/js/ch/maenulabs/rest/angular/service/module.js
+++ src/main/js/ch/maenulabs/rest/angular/service/module.js
@@ -3,6 +3,7 @@
  * The resource create.
  *
  * @module ch.maenulabs.rest.angular.service
+ * @requires ch.maenulabs.rest.angular.resource
  */
 angular.module('ch.maenulabs.rest.angular.service', [
 	'ch.maenulabs.rest.angular.resource'
